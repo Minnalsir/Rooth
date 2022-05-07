49560956935d73dda66f6fa2893576aee2fca5d5
Leemy's <78458084+Leemys@users.noreply.github.com>
2022-05-07 10:07:52 -0300

    Arrumando alguns bugs de conexão...

--- mauri.js
+++ mauri.js
@@ -2,6 +2,7 @@ const qrcode = require('qrcode-terminal');
 const path = require('path');
 const fs = require('fs');
 const { Client ,LocalAuth } = require('whatsapp-web.js');
+const aspas = '```'
 const axios = require('axios')
 const client = new Client({ puppeteer: {  headless: true,  args: ['--no-sandbox']},authStrategy: new LocalAuth({ clientId: 'mauri' })});client.initialize();client.on('qr', qr => {qrcode.generate(qr, {small: true});})
 client.on('authenticated', () => {console.log('AUTENTICADO!!');});client.on('ready', () => {console.log(`ESSE BOT ESTA CONECTADO TAMBÉM EM localhost:3000`);});client.on('message', async mauri => {console.log(`Mensagem De - ${mauri.body} `);
@@ -9,7 +10,7 @@ client.on('authenticated', () => {console.log('AUTENTICADO!!');});client.on('rea
 //𝑭𝑨𝑪̧𝑨 𝑨𝑸𝑼𝑰 𝑺𝑬𝑼𝑺 𝑪𝑶𝑴𝑨𝑵𝑫𝑶𝑺//
 
 if (mauri.body === 'menu') {
-mauri.reply('Esse bot esta em construção...')
+mauri.reply(`${aspas}Esse bot esta em construção...${aspas}`)
 }
 
 })
