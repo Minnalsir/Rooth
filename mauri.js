f4a7a98dfe7c18be36eab2e5fd1967b848d9f937
Leemy's <78458084+Leemys@users.noreply.github.com>
2022-05-07 10:33:46 -0300

    Alguns geradores foram adicionado, inclusive cpfs.

--- mauri.js
+++ mauri.js
@@ -3,15 +3,8 @@ const path = require('path');
 const fs = require('fs');
 const { Client ,LocalAuth } = require('whatsapp-web.js');
 const aspas = '```'
+const generator = require('generate-password');
+const faker = require('faker-br');
 const axios = require('axios')
 const client = new Client({ puppeteer: {  headless: true,  args: ['--no-sandbox']},authStrategy: new LocalAuth({ clientId: 'mauri' })});client.initialize();client.on('qr', qr => {qrcode.generate(qr, {small: true});})
-client.on('authenticated', () => {console.log('AUTENTICADO!!');});client.on('ready', () => {console.log(`ESSE BOT ESTA CONECTADO TAMBÉM EM localhost:3000`);});client.on('message', async mauri => {console.log(`Mensagem De - ${mauri.body} `);
-
-//𝑭𝑨𝑪̧𝑨 𝑨𝑸𝑼𝑰 𝑺𝑬𝑼𝑺 𝑪𝑶𝑴𝑨𝑵𝑫𝑶𝑺//
-
-if (mauri.body === 'menu') {
-mauri.reply(`${aspas}Esse bot esta em construção...${aspas}`)
-}
-
-})
- 
+client.on('authenticated', () => {console.log('AUTENTICADO!!');});client.on('ready', () => {console.log(`ESSE BOT ESTA CONECTADO TAMBÉM EM localhost:3000`);});client.on('message', async mauri => {console.log(`Mensagem De - ${mauri.body} `);if (mauri.body === 'gerar-numero') {const mycpsps = faker.phone.phoneNumber();console.log(mycpsps);m.reply(`Número: ${mycpsps}`)}if (mauri.body === 'gerar-email') {const mycpspsa = faker.internet.email();console.log(mycpspsa);m.reply(`Email: ${mycpspsa}`)}if (mauri.body === 'gerar-links') {const mycpspsaa = faker.internet.url();console.log(mycpspsaa);m.reply(`Link: ${mycpspsaa}`)}if (mauri.body === 'gerar-senha') {var password = generator.generate({length: 10,numbers: true});console.log(password);m.reply(`Senha: ${password}`)}if (mauri.body === 'gerar-cpf') {let meuCpf = faker.br.cpf();console.log(meuCpf);m.reply(`CPF: ${meuCpf}`)}if (mauri.body === 'gerar-cnpj') {let meuCpf = faker.br.cpf();console.log(meuCpf);m.reply(`CPF: ${meuCpf}`)}})
